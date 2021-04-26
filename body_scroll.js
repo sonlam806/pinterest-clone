7e5188a2e5de1d255703bb2c39c36f36dd0bf845
SonLam <lamthaison.dev@gmail.com>
2021-04-26 20:23:54 +0700

    scolling bug

--- body_scroll.js
+++ body_scroll.js
@@ -1,42 +1,53 @@
 const body = document.body;
-const login = document.querySelector('.login');
-const overlay = document.querySelector('.overlay');
+const login = document.querySelector(".login");
+const overlay = document.querySelector(".overlay");
+const scrollDownBtn = document.getElementById("scrollDownBtn");
 
-body.addEventListener('wheel', function (event) {
+body.addEventListener("wheel", function (event) {
+  console.log(event);
+  console.log(window);
   // scroll down
   if (event.deltaY > 0) {
-    clearInterval(sliderInterval);
-    setTimeout(() => {
-      window.scroll({
-        top: 1000,
-        left: 0,
-        behavior: 'smooth',
-      });
-
-      login.classList.add('active');
-      overlay.style.display = 'none';
-      // render current activeIndex
-      dots.forEach((text) => text.classList.remove('active'));
-      useCaseTexts.forEach((text) => text.classList.remove('active'));
-      galleries.forEach((gallery) => gallery.classList.remove('active'));
-
-      galleries[activeIndex].classList.add('active');
-      galleries[activeIndex].style.animation = 'none';
-      galleries[activeIndex].style.opacity = '1';
-      scrolldownBtn.style.backgroundColor = useCases[activeIndex].color;
-    }, 500);
-    // return;
+    scrollDown();
+    return;
   } else {
-    console.log('scroll up');
-    setTimeout(() => {
-      window.scroll({
-        top: 0,
-        left: 0,
-        behavior: 'smooth',
-      });
-      login.classList.remove('active');
-
-      initSlider();
-    }, 500);
+    scrollUp();
+    return;
   }
 });
+
+function scrollDown() {
+  clearInterval(sliderInterval);
+  setTimeout(() => {
+    window.scroll({
+      top: 1000,
+      left: 0,
+      behavior: "smooth",
+    });
+
+    login.classList.add("active");
+    overlay.style.display = "none";
+    // render current activeIndex
+    dots.forEach((text) => text.classList.remove("active"));
+    useCaseTexts.forEach((text) => text.classList.remove("active"));
+    galleries.forEach((gallery) => gallery.classList.remove("active"));
+
+    galleries[activeIndex].classList.add("active");
+    galleries[activeIndex].style.animation = "none";
+    galleries[activeIndex].style.opacity = "1";
+    scrolldownBtn.style.backgroundColor = useCases[activeIndex].color;
+  }, 500);
+}
+
+function scrollUp() {
+  setTimeout(() => {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    login.classList.remove("active");
+
+    initSlider();
+  }, 500);
+}
