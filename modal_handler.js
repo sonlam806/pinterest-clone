62153d2599de1444fcff68498a8221d5bfb80a3c
SonLam <thaisonlamsp@gmail.com>
2021-05-12 08:32:01 +0700

    add controller for login modal

--- modal_handler.js
+++ modal_handler.js
@@ -1,10 +1,12 @@
-const signUpBtn = document.getElementById("sign-up");
-const loginBtn = document.getElementById("log-in");
-const signUpModal = document.getElementById("sign-up-modal");
+const openSignUpModalBtn = document.getElementById("sign-up");
 const closeSignupBtn = document.getElementById("close-sign-up-btn");
+const signUpModal = document.getElementById("sign-up-modal");
+const openLogInModalBtn = document.getElementById("log-in");
+const closeLogInModalBtn = document.getElementById("close-log-in-btn");
+const loginModal = document.getElementById("log-in-modal");
 
 // toggle SignUp modal
-signUpBtn.addEventListener("click", () => {
+openSignUpModalBtn.addEventListener("click", () => {
   // prevent document scrolling event
   body.removeEventListener("wheel", scroll);
   // show the modal
@@ -18,5 +20,22 @@ closeSignupBtn.addEventListener("click", () => {
 
   signUpModal.classList.remove("show");
   signUpModal.classList.add("hide");
+});
+
+// toggle LogIn Modal
+openLogInModalBtn.addEventListener("click", () => {
+  // prevent document scrolling event
+  body.removeEventListener("wheel", scroll);
+  // show the modal
+  loginModal.classList.remove("hide");
+  loginModal.classList.add("show");
+});
+
+closeLogInModalBtn.addEventListener("click", () => {
+  // re-initialize wheel scroll event
+  body.addEventListener("wheel", scroll);
+
+  loginModal.classList.remove("show");
+  loginModal.classList.add("hide");
 });
 
