53709a49e530cc2fc44531c765c54c830bdd90f3
SonLam <thaisonlamsp@gmail.com>
2021-05-18 10:16:24 +0700

    toggle search icon

--- search-handler.js
+++ search-handler.js
@@ -1,10 +1,13 @@
 const searchBoxContainer = document.getElementById("searchBoxContainer");
+const searchIcon = document.getElementById("iconSearch");
 
 function searchFocus() {
   searchBoxContainer.style.border = "2px solid dodgerblue";
+  searchIcon.style.display = "none";
 }
 
 function searchBlur() {
   searchBoxContainer.style.border = "none";
+  searchIcon.style.display = "flex";
 }
 
