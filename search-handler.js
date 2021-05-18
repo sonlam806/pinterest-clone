46e4186c8b8c9f2e6644d4554e52674ee90a9a85
SonLam <thaisonlamsp@gmail.com>
2021-05-18 10:13:14 +0700

    add search script handler

---
+++ search-handler.js
@@ -0,0 +1,10 @@
+const searchBoxContainer = document.getElementById("searchBoxContainer");
+
+function searchFocus() {
+  searchBoxContainer.style.border = "2px solid dodgerblue";
+}
+
+function searchBlur() {
+  searchBoxContainer.style.border = "none";
+}
+
